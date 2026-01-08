457b5f76ff0e867e06716e46428e783c7718d111
LUCIANO PAIVA CAVALCANTI <98571732+Cavalcantiexpresso@users.noreply.github.com>
2026-01-08 19:46:19 -0300

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
+
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://luciano-landing.vercel.app',
-
-
+  site: 'https://cavalcanteexpresso.github.io/luciano-landing/',
+  base: '/luciano-landing/',
   integrations: [icon()],
 });
